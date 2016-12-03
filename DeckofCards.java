0b31ad99c8d02487edfc7324fc5ec425200eccf6
brianarch82 <bmolson@mchsi.com>
2016-12-03 16:25:48 -0600

    Update DeckofCards.java

--- DeckofCards.java
+++ DeckofCards.java
@@ -1,34 +1,28 @@
-import java.util.*;
+mport java.util.*;
  
 public class DeckofCards {
+		//52 Cards are kept in a array list
         ArrayList<Card> cards = new ArrayList<Card>();
-       
+       //Each card is assigned values and a suit
         String[] values = {"A","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
         String[] suit = {"Clubs", "Spades", "Diamonds", "Hearts"};
        
-        static boolean firstThread = true;
+        //Constructor to create the deck of cards
         public DeckofCards(){
+        	//Making use a nested for loop to assign suit each value
                 for (int i = 0; i<suit.length; i++) {
                         for(int j=0; j<values.length; j++){
+                        	//Card being created and saved to the ArrayList of cards.
                                 this.cards.add(new Card(suit[i],values[j]));
                         }
                 }
-                //shuffle the deck when its created
+                //Shuffle the deck when its created
                 Collections.shuffle(this.cards);
                
         }
        
+        //Returns the deck of cards
         public ArrayList<Card> getDeck(){
                 return cards;
-        }
-       
-       // public static void main(String[] args){
-             //   DeckofCards deck = new DeckofCards();
-               
-                //print out the deck.
-           //     System.out.println(deck.getDeck());
-       // }
-       
+        }   
 }
- 
- 
